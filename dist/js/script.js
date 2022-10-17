5c0b23237177133969b0c777c857f5afd4ee3c01
dsc-ubg <lombokdsc.indonesia@gmail.com>
2022-10-18 00:49:57 +0800

    update structure

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,17 +1,20 @@
 var dir = "./dist/mading";
 let fileextension = ".html"
-$.ajax({
-    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
-    method: 'GET',
-    url: dir,
-    success: function (data) {
-        $(data).find("a:contains(" + fileextension + ")").each(function () {
-            var filename = this.href.replace(window.location.host, "").replace("http:///", "");
-            $("#container-card").append(`<div class="w-[30%] h-80 overflow-x-hidden rounded-xl border">
-            <iframe src="./${filename}" class="w-full h-full" frameborder="0"></iframe>
-          </div>`)
-            // console.log(filename);
-            // $("body").append($("<img src=" + dir + filename + "></img>"));
-        });
-    }
-});
+
+function getListData(){
+    $.ajax({
+        //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+        method: 'GET',
+        url: 'https://gdsc-ubg-be.herokuapp.com/get-list',
+        success: function (data) {
+            console.log();
+            data.data.forEach(el => {
+                    $("#container-card").append(`<div style="width: 30%; min-height: 15rem; overflow: hidden; border-radius: 16px;" class="w-[30%] h-80 overflow-x-hidden rounded-xl border">
+                    <iframe src="https://gdsc-ubg-be.herokuapp.com/get-page/${el}" style="width:100%; height:100%;" frameborder="0"></iframe>
+                  </div>`)
+            });
+        }
+    });
+}
+
+getListData()
